7ea36c42e2c4383bdb5cc79a22d699a6b3baf051
unon4all <unon4all@gmail.com>
2026-09-15 19:35:49 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/nigeriamart-website/',
+
   plugins: [react(), tailwindcss()],
+
   build: {
     target: 'es2020',
     cssMinify: true,
